9558a89bda9b39ee9b6dbabef6132ed8b19a0b7b
camylla-araujo <camylla.a_@hotmail.com>
2022-09-30 23:25:59 -0300

    Adicionando novos arquivos

---
+++ eventos.js
@@ -0,0 +1,39 @@
+fetch('https://servicodados.ibge.gov.br/api/v1/localidades/regioes')
+    .then( (resposta) => resposta.json())
+    .then( (regioes) => {
+        regioes.forEach ( (cadaRegiao) => {
+            document.getElementById('regiao').innerHTML += `
+            <option value="${cadaRegiao.id}">${cadaRegiao.nome}</option>
+            `;
+        });
+       
+    });
+
+function selecionarEstado() {
+    let idEstado = document.getElementById('regiao').value 
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/regioes/${idEstado}/estados`)
+    .then( (resposta) => resposta.json())
+    .then( (estados) => {
+        estados.forEach ( (cadaEstado) => {
+            document.getElementById('estado').innerHTML += `
+            <option value = "${cadaEstado.id}">${cadaEstado.nome}</option>
+            `;
+        });
+       
+    });
+}
+
+function selecionarCidade() {
+    let idCidade = document.getElementById('estado').value
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${idCidade}/microrregioes`)
+    .then( (resposta) => resposta.json())
+    .then( (cidade) => {
+        cidade.forEach ( (cadaCidade) => {
+            document.getElementById('cidade').innerHTML += `
+            <option value = "${cadaCidade.id}">${cadaCidade.nome}</option>
+            `;
+        });
+       
+    });
+}
+
